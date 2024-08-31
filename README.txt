eb751215194b1a4d153339bb6cd239061eb8a24b
Ethan Baldwin <eab77806@uga.edu>
2024-08-31 17:41:33 -0400

    upload better template

--- README.txt
+++ README.txt
@@ -1,31 +1,22 @@
-Miniport by HTML5 UP
+Minimaxing by HTML5 UP
 html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-A super simple responsive portfolio template. It's only a single page because hey, sometimes
-you don't need a half dozen pages to make your point.
-
-Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
-you can use for pretty much whatever.
-
-(* = Not included)
+The very first responsive site template I ever made on 5grid (then skelJS, then Skel,
+and finally Responsive Tools). It's clean, minimal, and so generic you can use it for
+pretty much anything.
 
 Feedback, bug reports, and comments are not only welcome, but strongly encouraged :)
 
 AJ
 aj@lkn.io | @ajlkn
 
-PS: Not sure how to get that contact form working? Give formspree.io a try (it's awesome).
-
 
 Credits:
 
 	Demo Images:
 		Unsplash (unsplash.com)
-
-	Icons:
-		Font Awesome (fontawesome.io)
 
 	Other:
 		jQuery (jquery.com)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
 /*
-	Miniport by HTML5 UP
+	Minimaxing by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+	var $window = $(window),
+		$body = $('body');
 
 	// Breakpoints.
 		breakpoints({
@@ -18,17 +17,35 @@
 			small:   [ null,      '736px'  ]
 		});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	// Nav.
 
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+					'<span class="title">' + $('#logo').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Navigation Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
 
 })(jQuery);
